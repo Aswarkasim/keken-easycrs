eefd97b31bde42d62813efacc413d665b55c24f2
unknown <aswarkasim@gmail.com>
2022-09-28 19:31:57 +0700

    banner

--- resources/views/home/home/index.blade.php
+++ resources/views/home/home/index.blade.php
@@ -14,7 +14,7 @@
       <div class="container">
         <div class="carousel-caption text-left">
           <h1>{{$item->topik}}</h1>
-          <p>{{$item->desc}}</p>
+          <p>{!!$item->desc!!}</p>
           {{-- <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p> --}}
         </div>
       </div>
